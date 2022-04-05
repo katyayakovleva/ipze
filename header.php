8d5b1d89d1ba962f4d301159a5f29709aff2729c
sobolv_a <lera.sobol.26@gmail.com>
2022-04-05 23:06:11 +0300

    Додавання логотипу та зміни мови

--- header.php
+++ header.php
@@ -23,38 +23,56 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'ipze' ); ?></a>
+<!--<div id="page" class="site">-->
+<!--	<a class="skip-link screen-reader-text" href="#primary">--><?php //esc_html_e( 'Skip to content', 'ipze' ); ?><!--</a>--> \
+    <header>
+        <div class="container">
+            <div class="row logo-row">
+                <div class="col-sm-12">
+                    <a href="" class="logo">
+                        <img src="<?php echo get_template_directory_uri(); ?> . /assets/img/ipze_logo.svg" alt="ІПЗЕ">
+                    </a>
+                    <div class="lans">
+                        <span class="lans-text">
+                            <a href="" class="">Укр.</a>
+                            <span class="stick">|</span>
+                            <a href="" class="">Eng.</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$ipze_description = get_bloginfo( 'description', 'display' );
-			if ( $ipze_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $ipze_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ipze' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'main-menu',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+<!--	<header id="masthead" class="site-header">-->
+<!--		<div class="site-branding">-->
+<!--			--><?php
+//			the_custom_logo();
+//			if ( is_front_page() && is_home() ) :
+//				?>
+<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
+<!--				--><?php
+//			else :
+//				?>
+<!--				<p class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></p>-->
+<!--				--><?php
+//			endif;
+//			$ipze_description = get_bloginfo( 'description', 'display' );
+//			if ( $ipze_description || is_customize_preview() ) :
+//				?>
+<!--				<p class="site-description">--><?php //echo $ipze_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?><!--</p>-->
+<!--			--><?php //endif; ?>
+<!--		</div><!-- .site-branding -->-->
+<!---->
+<!--		<nav id="site-navigation" class="main-navigation">-->
+<!--			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">--><?php //esc_html_e( 'Primary Menu', 'ipze' ); ?><!--</button>-->
+<!--			--><?php
+//			wp_nav_menu(
+//				array(
+//					'theme_location' => 'main-menu',
+//					'menu_id'        => 'primary-menu',
+//				)
+//			);
+//			?>
+<!--		</nav><!-- #site-navigation -->-->
+<!--	</header><!-- #masthead -->-->
